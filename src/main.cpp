f9aefa78293a06cc204549cf5c9264d7c31133a7
Victor Z. Shi <vzshi@pm.me>
2022-01-31 23:04:20 -0500

    Build without SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,35 @@
-#define SDL_MAIN_HANDLED
+//#define SDL_MAIN_HANDLED
+//
+//#include "SDL.h"
+//
+//int main() {
+//  const int SCREEN_WIDTH = 640;
+//  const int SCREEN_HEIGHT = 480;
+//  const int STAY_ALIVE = 3000;
+//
+//  SDL_Init(SDL_INIT_VIDEO);
+//
+//  SDL_Window *window =
+//      SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED,
+//                       SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+//
+//  SDL_Renderer *renderer =
+//      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+//  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+//  SDL_RenderClear(renderer);
+//  SDL_RenderPresent(renderer);
+//
+//  SDL_Delay(STAY_ALIVE);
+//
+//  SDL_DestroyWindow(window);
+//  SDL_Quit();
+//
+//  return 0;
+//}
 
-#include "SDL.h"
+#include <iostream>
 
 int main() {
-  const int SCREEN_WIDTH = 640;
-  const int SCREEN_HEIGHT = 480;
-  const int STAY_ALIVE = 3000;
-
-  SDL_Init(SDL_INIT_VIDEO);
-
-  SDL_Window *window =
-      SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-
-  SDL_Renderer *renderer =
-      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
-
-  SDL_Delay(STAY_ALIVE);
-
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
+  std::cout << "Hello World!\n";
   return 0;
 }
