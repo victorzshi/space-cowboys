4d6727be3166002e15f384441aa2cb415b68981b
Victor Z. Shi <vzshi@pm.me>
2022-01-31 23:09:44 -0500

    Add SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,23 @@
 //#define SDL_MAIN_HANDLED
-//
-//#include "SDL.h"
-//
-//int main() {
-//  const int SCREEN_WIDTH = 640;
-//  const int SCREEN_HEIGHT = 480;
-//  const int STAY_ALIVE = 3000;
-//
-//  SDL_Init(SDL_INIT_VIDEO);
-//
-//  SDL_Window *window =
-//      SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED,
-//                       SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-//
-//  SDL_Renderer *renderer =
-//      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-//  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-//  SDL_RenderClear(renderer);
-//  SDL_RenderPresent(renderer);
-//
-//  SDL_Delay(STAY_ALIVE);
-//
-//  SDL_DestroyWindow(window);
-//  SDL_Quit();
-//
-//  return 0;
-//}
 
-#include <iostream>
+#include "SDL.h"
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(int argc, char *argv[]) {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window *window = SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+
+  SDL_Delay(3000);
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
 }
