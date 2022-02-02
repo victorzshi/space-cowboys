513ebe4619197d13b283cccd5018a54136949c1d
Victor Z. Shi <vzshi@pm.me>
2022-02-01 20:46:53 -0500

    Add Game class

---
+++ src/game/game.cpp
@@ -0,0 +1,26 @@
+#include "game.hpp"
+
+Game::Game() {
+  window = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, m_SCREEN_WIDTH,
+                            m_SCREEN_HEIGHT, 0);
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+}
+
+bool Game::init() { 
+  return SDL_Init(SDL_INIT_VIDEO) == 0; 
+}
+
+void Game::start() {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+
+  SDL_Delay(m_STAY_ALIVE);
+}
+
+void Game::stop() {
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,18 @@
-#define SDL_MAIN_HANDLED
+#include "game/game.hpp"
 
-#include "SDL.h"
+int main(int argc, char* argv[]) {
+  // Ignore unused parameters
+  (void)argc;
+  (void)argv;
 
-int main() {
-  const int SCREEN_WIDTH = 640;
-  const int SCREEN_HEIGHT = 480;
-  const int STAY_ALIVE = 3000;
+  if (Game::init())
 
-  SDL_Init(SDL_INIT_VIDEO);
+  {
+    Game game = Game();
 
-  SDL_Window *window =
-      SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-
-  SDL_Renderer *renderer =
-      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
-
-  SDL_Delay(STAY_ALIVE);
-
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    game.start();
+    game.stop();
+  }
 
   return 0;
 }
