ecb192ee4ae98c480fbaaa4aa31948de08e07093
Victor Z. Shi <vzshi@pm.me>
2022-02-19 15:25:21 -0500

    Remove inline function

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -4,7 +4,7 @@
 
 class Utils {
  public:
-  static inline bool isEqual(float a, float b, float epsilon = 1.0f / 1000) {
+  static bool isEqual(float a, float b, float epsilon = 1.0f / 1000) {
     return abs(a - b) <= epsilon * (abs(a) + abs(b) + 1.0f);
   }
 };
--- src/vector2/vector2.test.cc
+++ src/vector2/vector2.test.cc
@@ -8,7 +8,7 @@
 Vector2 v = Vector2(1.0f, 2.0f);
 Vector2 w = Vector2(3.0f, 4.0f);
 
-TEST_CASE("Vector2 add", "[vec2]") {
+TEST_CASE("Vector2 add", "[vector2]") {
   Vector2 u = v + w;
   REQUIRE(u == Vector2(4.0f, 6.0f));
   REQUIRE(u != Vector2(4.1f, 6.0f));
@@ -20,7 +20,7 @@ TEST_CASE("Vector2 add", "[vec2]") {
   REQUIRE(u != Vector2(8.0f, 12.1f));
 }
 
-TEST_CASE("Vector2 subtract", "[vec2]") {
+TEST_CASE("Vector2 subtract", "[vector2]") {
   Vector2 u = v - w;
   REQUIRE(u == Vector2(-2.0f, -2.0f));
   REQUIRE(u != Vector2(-2.1f, -2.0f));
@@ -32,34 +32,34 @@ TEST_CASE("Vector2 subtract", "[vec2]") {
   REQUIRE(u != Vector2(0.0f, 0.1f));
 }
 
-TEST_CASE("Vector2 multiply", "[vec2]") {
+TEST_CASE("Vector2 multiply", "[vector2]") {
   Vector2 u = v * 1.5f;
   REQUIRE(u == Vector2(1.5f, 3.0f));
   REQUIRE(u != Vector2(1.6f, 3.0f));
   REQUIRE(u != Vector2(1.5f, 3.1f));
 }
 
-TEST_CASE("Vector2 divide", "[vec2]") {
+TEST_CASE("Vector2 divide", "[vector2]") {
   Vector2 u = v / 2.0f;
   REQUIRE(u == Vector2(0.5f, 1.0f));
   REQUIRE(u != Vector2(0.6f, 1.0f));
   REQUIRE(u != Vector2(0.5f, 1.1f));
 }
 
-TEST_CASE("Vector2 length", "[vec2]") {
+TEST_CASE("Vector2 length", "[vector2]") {
   float length = v.length();
   REQUIRE(Utils::isEqual(length, sqrt(5)));
   REQUIRE(!Utils::isEqual(length, sqrt(4)));
   REQUIRE(!Utils::isEqual(length, sqrt(6)));
 }
 
-TEST_CASE("Vector2 normalize", "[vec2]") {
+TEST_CASE("Vector2 normalize", "[vector2]") {
   float length = v.length();
   Vector2 u = v.normalize();
   REQUIRE(u == Vector2(v.x / length, v.y / length));
 }
 
-TEST_CASE("Vector2 limit", "[vec2]") {
+TEST_CASE("Vector2 limit", "[vector2]") {
   float length = v.length();
   float n = 0.5f;
   Vector2 u = v.limit(n);
