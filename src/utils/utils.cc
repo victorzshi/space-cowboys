0fd549b9868a3fd5c8fae16b814055ecfcc61085
Victor Z. Shi <vzshi@pm.me>
2022-02-19 15:21:44 -0500

    Use inline function

--- src/utils/utils.cc
+++
@@ -1,7 +0,0 @@
-#include "utils.h"
-
-#include <cmath>
-
-bool Utils::isEqual(float a, float b, float epsilon) {
-  return abs(a - b) <= epsilon * (abs(a) + abs(b) + 1.0f);
-}
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <cmath>
+
 class Utils {
  public:
-  static bool isEqual(float a, float b, float epsilon = 1.0f / 1000);
+  static inline bool isEqual(float a, float b, float epsilon = 1.0f / 1000) {
+    return abs(a - b) <= epsilon * (abs(a) + abs(b) + 1.0f);
+  }
 };
