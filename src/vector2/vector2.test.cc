494cfba6c78f91db87ce91813d34cf7fa180ee89
victorzshi <victorzshi@users.noreply.github.com>
2022-02-19 20:29:40 +0000

    Format code

--- src/vector2/vector2.test.cc
+++ src/vector2/vector2.test.cc
@@ -11,46 +11,46 @@ Vector2 w = Vector2(3.0f, 4.0f);
 TEST_CASE("Vector2 add", "[vector2]") {
   Vector2 u = v + w;
   REQUIRE(u == Vector2(4.0f, 6.0f));
-  //REQUIRE(u != Vector2(4.1f, 6.0f));
-  //REQUIRE(u != Vector2(4.0f, 6.1f));
+  // REQUIRE(u != Vector2(4.1f, 6.0f));
+  // REQUIRE(u != Vector2(4.0f, 6.1f));
 
   u += u;
   REQUIRE(u == Vector2(8.0f, 12.0f));
-  //REQUIRE(u != Vector2(8.1f, 12.0f));
-  //REQUIRE(u != Vector2(8.0f, 12.1f));
+  // REQUIRE(u != Vector2(8.1f, 12.0f));
+  // REQUIRE(u != Vector2(8.0f, 12.1f));
 }
 
 TEST_CASE("Vector2 subtract", "[vector2]") {
   Vector2 u = v - w;
   REQUIRE(u == Vector2(-2.0f, -2.0f));
-  //REQUIRE(u != Vector2(-2.1f, -2.0f));
-  //REQUIRE(u != Vector2(-2.0f, -2.1f));
+  // REQUIRE(u != Vector2(-2.1f, -2.0f));
+  // REQUIRE(u != Vector2(-2.0f, -2.1f));
 
   u -= u;
   REQUIRE(u == Vector2(0.0f, 0.0f));
-  //REQUIRE(u != Vector2(0.1f, 0.0f));
-  //REQUIRE(u != Vector2(0.0f, 0.1f));
+  // REQUIRE(u != Vector2(0.1f, 0.0f));
+  // REQUIRE(u != Vector2(0.0f, 0.1f));
 }
 
 TEST_CASE("Vector2 multiply", "[vector2]") {
   Vector2 u = v * 1.5f;
   REQUIRE(u == Vector2(1.5f, 3.0f));
-  //REQUIRE(u != Vector2(1.6f, 3.0f));
-  //REQUIRE(u != Vector2(1.5f, 3.1f));
+  // REQUIRE(u != Vector2(1.6f, 3.0f));
+  // REQUIRE(u != Vector2(1.5f, 3.1f));
 }
 
 TEST_CASE("Vector2 divide", "[vector2]") {
   Vector2 u = v / 2.0f;
   REQUIRE(u == Vector2(0.5f, 1.0f));
-  //REQUIRE(u != Vector2(0.6f, 1.0f));
-  //REQUIRE(u != Vector2(0.5f, 1.1f));
+  // REQUIRE(u != Vector2(0.6f, 1.0f));
+  // REQUIRE(u != Vector2(0.5f, 1.1f));
 }
 
 TEST_CASE("Vector2 length", "[vector2]") {
   float length = v.length();
   REQUIRE(Utils::isEqual(length, sqrt(5)));
-  //REQUIRE(!Utils::isEqual(length, sqrt(4)));
-  //REQUIRE(!Utils::isEqual(length, sqrt(6)));
+  // REQUIRE(!Utils::isEqual(length, sqrt(4)));
+  // REQUIRE(!Utils::isEqual(length, sqrt(6)));
 }
 
 TEST_CASE("Vector2 normalize", "[vector2]") {
